c86a233afd31058c5cbb07672ab3e02e571bace0
Elise Phan <aliceconnic@gmail.com>
2019-03-28 23:07:24 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'navigate' do
   before do
     @post = Post.create(title: "My Post", description: "My post desc")
-    @author = Author.create(name: "Steven")
+    @author = @post.build_author(name: "Steven")
   end
 
   it 'shows the title on the show page in a h1 tag' do
